f50aa1c60c342dbb7ae2671367f369605b17cc4e
Swathinamburu1 <namburiswathi1@gmail.com>
2025-03-08 20:13:26 +0530

    Update Abstract Class.py

--- Abstract Class.py
+++ Abstract Class.py
@@ -1,45 +1,116 @@
+"""------ABSTRACT CLASS------"""
+"""--------1. Create an abstract class with abstract and non-abstract methods------------"""
+
 from abc import ABC, abstractmethod
- 
-class Polygon(ABC): #base class / super class
- 
+
+
+class MyAbstractClass(ABC):
+
     @abstractmethod
-    def noofsides(self):
+    def abstract_method(self):
         pass
- 
-class Triangle(Polygon): #subclass
- 
-    # overriding abstract method
-    def noofsides(self):
-        print("Triangle: I have 3 sides")
- 
-class Pentagon(Polygon): #subclass
- 
-    # overriding abstract method
-    def noofsides(self):
-        print("Pentagon: I have 5 sides")
- 
-class Hexagon(Polygon): #subclass
- 
-    # overriding abstract method
-    def noofsides(self):
-        print("Hexagon: I have 6 sides")
- 
-class Quadrilateral(Polygon): #subclass
- 
-    # overriding abstract method
-    def noofsides(self):
-        print("Quadrilateral: I have 4 sides")
- 
-# Driver code
-# Creating the objects to call the abstract method.
-R = Triangle()
-R.noofsides()
- 
-K = Quadrilateral()
-K.noofsides()
- 
-R = Pentagon()
-R.noofsides()
- 
-K = Hexagon()
-K.noofsides()
+
+    @staticmethod
+    def non_abstract_method():
+        print("This is a non-abstract method in the abstract class.")
+
+
+class ConcreteClass(MyAbstractClass):
+
+    def abstract_method(self):
+        print("Implemented the abstract method in the concrete class.")
+
+
+obj = ConcreteClass()
+obj.abstract_method()
+obj.non_abstract_method()
+
+
+"""-----------2. Create a sub class for an abstract class. Create an object in the child class for the  
+abstract class and access the non-abstract methods---------"""
+
+from abc import ABC, abstractmethod
+
+
+class MyAbstractClass(ABC):
+
+    @abstractmethod
+    def abstract_method(self):
+        pass
+
+    @staticmethod
+    def non_abstract_method():
+        print("This is a non-abstract method in the abstract class.")
+
+
+class ChildClass(MyAbstractClass):
+
+    def abstract_method(self):
+        print("Implemented the abstract method in the child class.")
+
+    def access_non_abstract_method(self):
+        self.non_abstract_method()
+
+
+obj = ChildClass()
+obj.abstract_method()
+obj.access_non_abstract_method()
+
+
+"""---------3. Create an instance for the child class in child class and call abstract methods ------------"""
+
+from abc import ABC, abstractmethod
+
+
+class MyAbstractClass(ABC):
+
+    @abstractmethod
+    def abstract_method(self):
+        pass
+
+    @staticmethod
+    def non_abstract_method():
+        print("This is a non-abstract method in the abstract class.")
+
+
+class ChildClass(MyAbstractClass):
+
+    def abstract_method(self):
+        print("Implemented the abstract method in the child class.")
+
+    @staticmethod
+    def call_abstract_method_in_child():
+        child_instance = ChildClass()
+        child_instance.abstract_method()
+
+
+ChildClass.call_abstract_method_in_child()
+
+"""-----------4. Create an instance for the child class in child class and call non-abstract methods-----------"""
+
+from abc import ABC, abstractmethod
+
+
+class MyAbstractClass(ABC):
+
+    @abstractmethod
+    def abstract_method(self):
+        pass
+
+    @staticmethod
+    def non_abstract_method():
+        print("This is a non-abstract method in the abstract class.")
+
+
+class ChildClass(MyAbstractClass):
+
+    def abstract_method(self):
+        print("Implemented the abstract method in the child class.")
+
+    @staticmethod
+    def call_non_abstract_method_in_child():
+        child_instance = ChildClass()
+        child_instance.non_abstract_method()
+
+
+ChildClass.call_non_abstract_method_in_child()
